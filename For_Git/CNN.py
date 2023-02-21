76f5996af725369ec4771c1bb230b5718bec911f
MRutland <MRutland0@gmail.com>
2023-02-21 18:01:56 +0000

    altered imports and tensorflow installation

--- For_Git/CNN.py
+++ For_Git/CNN.py
@@ -45,17 +45,17 @@ validation_data = validation_datagen.flow_from_directory(
 
 # Define the neural network architecture
 model = keras.Sequential([
-    keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(512, activation='relu'),
-    keras.layers.Dense(1, activation='sigmoid')
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
 ])
 
 # Compile the model with an appropriate loss function and optimizer
